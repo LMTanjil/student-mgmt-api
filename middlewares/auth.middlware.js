d0de9a73411cbeac5a7e2099f9505b0a254edfee
tanjil <tanjilalhussain@gmail.com>
2026-08-20 20:31:57 +0600

    feat: add jwt auth middleware to protect routes

---
+++ middlewares/auth.middlware.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+
+export const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if (!authHeader || authHeader.startsWith('Bearer ')) {
+        return res.status(401).send({
+            message: 'Unauthorized, no token provided',
+        })
+    }
+    const token = authHeader.split(' ')[1]
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRECT)
+        req.userId = decoded.id;
+        next()
+
+    }catch(err){
+        res.status(401).send({
+            message: 'Invalid or expired token',
+            error: err.message
+        })
+    }
+}
